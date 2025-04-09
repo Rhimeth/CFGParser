e4e3a1ee3e7575d1f091a453a24f18f29459330b
dubemntiwunka <61565714+Rhimeth@users.noreply.github.com>
2025-04-09 17:14:54 -0400

    Delete src directory

--- src/build/CMakeFiles/CFGParser.dir/CFGParser_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CFGParser.dir/CFGParser_autogen/mocs_compilation.cpp.o: \
- /home/dubem/cfg_parser/build/CFGParser_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- src/build/CMakeFiles/CFGParser_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CFGParser_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CFGParser_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
